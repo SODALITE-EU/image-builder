e83cfa003cbffac0f911f3143839e3c3d1bbf47b
Paul Mundt <paul.mundt@adaptant.io>
2020-07-28 16:46:38 +0200

    Support local build context with image builder CLI
    In order to build with a local build context or Dockerfile, each must be
    passed through to the image builder container as an additional volume
    mount. Update the image-builder-cli.sh script to handle this directly.

--- image-builder-cli.sh
+++ image-builder-cli.sh
@@ -9,7 +9,22 @@ get_abs_filename() {
 
 input=$(get_abs_filename $1)
 
+get_extra_volume_mounts() {
+  # $1 : input file
+
+  local_dockerfile=$(grep file:\/\/ $1 | sed -e 's/^.*file:\/\///')
+  local_build_context=$(grep path: $1 | sed -e 's/^.*path:[ \t]*//')
+
+  extra_mounts=""
+
+  [ ! -z $local_dockerfile ] && extra_mounts="$extra_mounts -v $local_dockerfile:$local_dockerfile"
+  [ ! -z $local_build_context ] && extra_mounts="$extra_mounts -v $local_build_context:$local_build_context"
+
+  echo $extra_mounts
+}
+
 docker run -it -v $input:/input.yaml \
 	   -v /var/run/docker.sock:/var/run/docker.sock \
+	   $(get_extra_volume_mounts $1) \
 	   --net=host \
 	   sodaliteh2020/image-builder-cli
