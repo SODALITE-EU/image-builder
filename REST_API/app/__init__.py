79a7269c2d96027ec0d6a789a720eabc495fa17c
Mihael Trajbarič <26118837+mihaTrajbaric@users.noreply.github.com>
2021-03-12 09:08:41 +0100

    Remove unnecesary endpoints (#24)

--- REST_API/app/__init__.py
+++ REST_API/app/__init__.py
@@ -3,23 +3,16 @@
 from flask import Blueprint
 from flask_restplus import Api
 
-from .main.controller.auth_controller import api as auth_ns
 from .main.controller.build_controller import api as build_ns
 from .main.controller.info_controller import api as info_ns
-from .main.controller.user_controller import api as user_ns
-from .main.util.dto import authorizations as auth
 
 blueprint = Blueprint('api', __name__)
 
 api = Api(blueprint,
           title='SODALITE image builder REST API',
-          version='0.3.0',
-          description='RESTful tool for building docker images',
-          security='Bearer token',
-          authorizations=auth
+          version='0.3.2',
+          description='RESTful tool for building docker images'
           )
 
-api.add_namespace(user_ns, path='/user')
-api.add_namespace(auth_ns)
 api.add_namespace(build_ns, path='/build')
 api.add_namespace(info_ns, path='/info')
